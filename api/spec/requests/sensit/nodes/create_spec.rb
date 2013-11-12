de3c8d7d5f6404db8195d9c6568dfb5997626376
Chris Waddington <cwaddington@solinkcorp.com>
2013-11-12 09:26:25 -0500

    "minor changes" to merge in work

--- api/spec/requests/sensit/nodes/create_spec.rb
+++ api/spec/requests/sensit/nodes/create_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 describe "POST sensit/nodes#create" do
-
+   # {
+   #    "node":{
+   #       "name":"my nodes",
+   #       "parent_id":1,//(optional) (associate parent child relationships) (self referential association)
+   #       "description":"A description of the node"//(optional)
+   #    }
+   # }
 
 	it "" do
 		params = {}
