5eac40aa28cd987b7338f48d24149c56cf0ceb47
Chris Waddington <cwadding@gmail.com>
2015-08-27 22:25:58 -0400

    require simplecov before requiring sensit_messenger for simple cov to track files

--- src/api/messenger/spec/spec_helper.rb
+++ src/api/messenger/spec/spec_helper.rb
@@ -1,10 +1,12 @@
 require 'rubygems'
 require 'bundler/setup'
 
-require 'sensit_messenger' # and any other gems you need
-
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+	add_group "lib", "lib"
+end
+
+require 'sensit_messenger' # and any other gems you need
 
 RSpec.configure do |config|
   # some (optional) config here
